03116760d6540c90ab9d2c8d537bea9b5ecfe0b6
Goutamrana2006 <gr2856466@gmail.com>
2026-03-17 22:28:19 +0530

    Delete Backend directory

--- Backend/models/OTP.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("OTP", new mongoose.Schema({
-  userId: mongoose.Schema.Types.ObjectId,
-  code: String,
-  expiresAt: Date,
-}));
